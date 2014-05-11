e6e11c314be803e482e9c315b8719803a2db9533
ZUTAOWU n8975698 <zutao.wu@connect.qut.edu.au>
2014-05-11 15:54:42 +1000

    + add NGramNodeTest.java + add NGramStoreTest.java

---
+++ src/assign2/ngram/NGramStoreTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package assign2.ngram;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author ZUTAOWU n8975698
+ *
+ */
+public class NGramStoreTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
