8e6812e079fe550ae325756b9f5dc73542104aa2
ZUTAOWU n8975698 <zutao.wu@connect.qut.edu.au>
2014-05-11 13:25:34 +1000

    ! update NGramStore.java | Finish all method basically

--- src/assign2/ngram/NGramStore.java
+++ src/assign2/ngram/NGramStore.java
@@ -1,48 +1,51 @@
 package assign2.ngram;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author ZUTAOWU n8975698
- * 
  */
 public class NGramStore implements NGramMap {
+	
+	private Map<String, NGramContainer> ngramMap;
+	
+	public NGramStore() {
+		ngramMap = new HashMap<String, NGramContainer>();
+	}
 
 	/**
 	 * @see assign2.ngram.NGramMap#addNGram(assign2.ngram.NGramContainer)
-	 * @author ZUTAOWU n8975698
 	 */
 	@Override
 	public void addNGram(NGramContainer ngram) {
-
+		ngramMap.put(ngram.getContext(), ngram);
 	}
 
 	/**
-	 * 
 	 * @see assign2.ngram.NGramMap#removeNGram(java.lang.String)
-	 * @author ZUTAOWU n8975698
 	 */
 	@Override
 	public void removeNGram(String context) {
-
+		ngramMap.remove(context);
 	}
 
 	/**
-	 * 
 	 * @see assign2.ngram.NGramMap#getNGram(java.lang.String)
-	 * @author ZUTAOWU n8975698
 	 */
 	@Override
 	public NGramContainer getNGram(String context) {
-		return null;
+		NGramContainer nc = ngramMap.get(context);
+		return nc;
 	}
 
 	/**
-	 * 
 	 * @see assign2.ngram.NGramMap#getNGramsFromService(java.lang.String, int)
-	 * @author ZUTAOWU n8975698
 	 */
 	@Override
 	public boolean getNGramsFromService(String context, int maxResults)
 			throws NGramException {
+		
 		return false;
 	}
 
