f386ea61fa25453205e9eaeba74184b5e2fc9e55
ZUTAOWU n8975698 <zutao.wu@connect.qut.edu.au>
2014-05-12 21:50:13 +1000

    ! update test NGramNode constructor

--- src/assign2/gui/ResultPanel.java
+++ src/assign2/gui/ResultPanel.java
@@ -5,6 +5,7 @@ package assign2.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -26,6 +27,7 @@ public class ResultPanel extends JPanel {
 		setLayout(new BorderLayout());
 		this.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 1, true),"Suggestion Results", TitledBorder.CENTER, TitledBorder.TOP));
 		resultDisplay = new JTextArea("This is a default text message");
+		resultDisplay.setPreferredSize(new Dimension(300,HEIGHT));
 		this.add(resultDisplay, BorderLayout.CENTER);
 		// setBackground(Color.BLACK);
 
