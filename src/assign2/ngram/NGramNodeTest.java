b74d0ffd5f2253df6723f2bced7742c5519ad632
iamkaushikhere <kaushikmuthukrishnan.simili@connect.qut.edu.au>
2014-05-14 01:04:19 +1000

    ! Updated 6 testcases for NGramNodeTest.java file along with Javadocs comments

--- src/assign2/ngram/NGramNodeTest.java
+++ src/assign2/ngram/NGramNodeTest.java
@@ -28,27 +28,11 @@ public class NGramNodeTest {
 	public static void tearDown() throws Exception {
 
 	}
-	
+		
 	/**
 	 * Test method : {@link assign2.ngram.NGramNode#NGramNode(java.lang.String[], java.lang.String[], java.lang.Double[])}.
 	 * <br/><br/> 
-	 * Test NGramNode constructor, when words, predictions and probabilities are null values and then throw NGramException
-	 * @throws NGramException
-	 */
-
-	@Test(expected = NGramException.class)
-	public void Test_Constructor1_Exception1() throws NGramException {
-		String[] words = null;
-		String[] predictions = null;
-		Double[] probabilities =null;
-		@SuppressWarnings("unused")
-		NGramNode node = new NGramNode(words, predictions, probabilities);
-	}
-	
-	/**
-	 * Test method : {@link assign2.ngram.NGramNode#NGramNode(java.lang.String[], java.lang.String[], java.lang.Double[])}.
-	 * <br/><br/> 
-	 * Test NGramNode constructor, when words, predictions and probabilities are null values and then throw NGramException
+	 * Test NGramNode first constructor, when words, predictions and probabilities show null value and then NGramException is thrown
 	 * @throws NGramException
 	 */
 	
@@ -61,6 +45,13 @@ public class NGramNodeTest {
 		NGramNode node = new NGramNode(words, predictions, probabilities);
 	}
 	
+	/**
+	 * Test method : {@link assign2.ngram.NGramNode#NGramNode(java.lang.String[], java.lang.String[], java.lang.Double[])}.
+	 * <br/><br/> 
+	 * Test NGramNode first constructor, when words are supplied with input values, predictions are displayed and probabilities show null value and then NGramException is thrown
+	 * @throws NGramException
+	 */
+	
 	@Test(expected = NGramException.class)
 	public void Test_Constructor1_Exception3() throws NGramException {
 		String[] words = {"Please", "Let", "Me", "Know"};
@@ -69,6 +60,13 @@ public class NGramNodeTest {
 		@SuppressWarnings("unused")
 		NGramNode node = new NGramNode(words, predictions, probabilities);
 	}
+	
+	/**
+	 * Test method : {@link assign2.ngram.NGramNode#NGramNode(java.lang.String[], java.lang.String[], java.lang.Double[])}.
+	 * <br/><br/> 
+	 * Test NGramNode first constructor, when words are supplied with input values, predictions has no input value and probabilities show null value and then NGramException is thrown
+	 * @throws NGramException
+	 */
 	
 	@Test(expected = NGramException.class)
 	public void Test_Constructor1_Exception4() throws NGramException {
@@ -79,6 +77,13 @@ public class NGramNodeTest {
 		NGramNode node = new NGramNode(words, predictions, probabilities);
 	}
 	
+	/**
+	 * Test method : {@link assign2.ngram.NGramNode#NGramNode(java.lang.String[], java.lang.String[], java.lang.Double[])}.
+	 * <br/><br/> 
+	 * Test NGramNode second constructor, when context has no input values supplied, predictions show null value and probabilities show null value and then NGramException is thrown
+	 * @throws NGramException
+	 */
+	
 	@Test(expected = NGramException.class)
 	public void Test_Constructor2_Exception1() throws NGramException {
 		String context = "";
@@ -87,6 +92,13 @@ public class NGramNodeTest {
 		@SuppressWarnings("unused")
 		NGramNode node = new NGramNode(context, predictions, probabilities);
 	}
+	
+	/**
+	 * Test method : {@link assign2.ngram.NGramNode#NGramNode(java.lang.String[], java.lang.String[], java.lang.Double[])}.
+	 * <br/><br/> 
+	 * Test NGramNode second constructor, when context is supplied with an input value, predictions are displayed and probabilities show null value and then NGramException is thrown
+	 * @throws NGramException
+	 */
 	
 	@Test(expected = NGramException.class)
 	public void Test_Constructor2_Exception2() throws NGramException {
@@ -97,11 +109,19 @@ public class NGramNodeTest {
 		NGramNode node = new NGramNode(context, predictions, probabilities);
 	}
 	
+	/**
+	 * Test method : {@link assign2.ngram.NGramNode#NGramNode(java.lang.String[], java.lang.String[], java.lang.Double[])}.
+	 * <br/><br/> 
+	 * Test NGramNode second constructor, when context is supplied with an input value, predictions are displayed and probabilities also are displayed and then NGramException is thrown
+	 * @throws NGramException
+	 */
+	
 	@Test(expected = NGramException.class)
 	public void Test_Constructor2_Exception3() throws NGramException {
 		String context = "Hello";
 		String[] predictions = {"How", "Are", "You"};
 		Double[] probabilities = {0.291898409, 0.229299928};
+		@SuppressWarnings("unused")
 		NGramNode node = new NGramNode(context, predictions, probabilities);
 	
     }
